d0c3e72c2aa2bb1a554dd215e69a1e20180c3027
alebrasi <alessandro.brasini1@gmail.com>
2021-07-21 20:26:06 +0200

    Added a device that isn't in the same network

---
+++ device5.py
@@ -0,0 +1,17 @@
+from device import device
+from IP import IP
+
+N_MEASURES = 4
+IP_ADDRESS_DEVICE = "192.168.1.6"
+SUBNET_MASK_DEVICE = "255.255.254.0"
+GATEWAY_ADDRESS = 'localhost'
+GATEWAY_PORT = 8000
+ID = 5
+
+ip = IP(IP_ADDRESS_DEVICE, SUBNET_MASK_DEVICE)
+
+dev = device(ID, ip)
+
+dev.generate_random_measures(N_MEASURES)
+dev.print_info()
+dev.send_data(GATEWAY_ADDRESS, GATEWAY_PORT)
